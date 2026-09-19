13e430345fb9705e1731624f3021beac19db2d0c
Habat025 <HabatDoneri025@gmail.com>
2026-09-19 13:10:33 +0300

    Add Flet app for user request submission
    Implemented a Flet application for user input and requests.

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
-name: Build Flet APK
-on:
-  workflow_dispatch:
+import flet as ft
+import requests
 
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v4
 
-      - name: Setup Python
-        uses: actions/setup-python@v5
-        with:
-          python-version: '3.10'
 
-      - name: Install Flet
-        run: |
-          pip install flet
+def main(pencere: ft.Page):
+    pencere.title = "Pear- İstek Kutusu"
+    pencere.vertical_alignment = ft.MainAxisAlignment.CENTER
+    pencere.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-      - name: Build APK (Yes Mode)
-        run: |
-          yes | flet build apk
+    def gonder(e):
+        URL = "https://ntfy.sh/pear_os_sinif_istekleri_025"
+        ad = yaziad.value
+        anlatim = yazitanitim.value
+        platform = yaziplatform.value
 
-      - name: Upload APK Artifact
-        uses: actions/upload-artifact@v4
-        with:
-          name: app-release.apk
-          path: build/apk/*.apk
+
+        if not ad or not ad.strip() or not anlatim or not anlatim.strip() or not platform or not platform.strip():
+            yazi.value = "Lütfen Her Alanı Doldurunuz \n"
+            pencere.update()
+            return
+        else:
+            yazi.value = "Talebiniz Uygulanmıştır, Habat size dediğinde uygulama hazırdır"
+
+        hazir_metin = f"Ad: {ad}, Uygulaması: {anlatim}, platform: {platform}"
+
+        try:
+            requests.post(URL, data=hazir_metin.encode("utf-8"))
+
+        except:
+            yazi.value = "İnternet Kopuk Veya Baka Bir Şey Oldu, Lütfen Tekrar Deneyin"
+
+
+
+        pencere.update()
+
+
+    yaziad = ft.TextField(label="Lütfen Adınızı Gİrin")
+
+    yazitanitim = ft.TextField(label="Uygulamanızın Anlatımı", multiline=True)
+
+    yaziplatform = ft.Dropdown(label="Platform",
+        options=[
+        ft.dropdown.Option("Mobil"),
+        ft.dropdown.Option("Pc(Bilgisayar)"),
+        ft.dropdown.Option("Çapraz Platform(Hem pc, hem mobil)")
+        ]
+        )
+
+    buton = ft.Button("Gönder", width=100, height=75, on_click=gonder)
+
+    yazi = ft.Text("", size=20)
+
+    pencere.add(yaziad, yazitanitim, yaziplatform, buton, yazi)
+
+ft.run(main)
